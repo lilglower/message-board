52f93d4ac9416edcfce802496e6140308de5cf2a
Adam Fowler <lilglower@hotmail.com>
2018-06-25 06:31:21 -0600

    Adding Support for file uploads / downloads

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,7 @@
-//import Controller from '@ember/controller';
+import Controller from '@ember/controller';
 import Ember from 'ember'
 
-export default Ember.Controller.extend({
+export default Controller.extend({
 	ajax: Ember.inject.service(),
 	loading : false,
 	message : "Hello World!",
@@ -21,17 +21,17 @@ export default Ember.Controller.extend({
 	getPosts(){
 		this.set('loading', true)
 		return this.get('ajax').request(this.endpoint + 'posts', {
-	      method: 'GET',
-	      data: {key : this.get('key')}
-	    })
-	    .then(result => {
-	    	this.set('posts', result.posts)
-	    	this.set('loading', false)
-	    })
-	    .catch(e => {
-	    	this.set('loading', false)
-	    	this.handleError(e)
-	    })
+				method: 'GET',
+				data: {key : this.get('key')}
+			})
+			.then(result => {
+				this.set('posts', result.posts)
+				this.set('loading', false)
+			})
+			.catch(e => {
+				this.set('loading', false)
+				this.handleError(e)
+			})
 	},
 	modify(post){
 		Ember.set(post, 'edit', !post.edit)
@@ -48,10 +48,14 @@ export default Ember.Controller.extend({
 		} catch(e){
 			return this.handleError(e)
 		}
-		return this.deletePost(this.get('change'), false)
+		return this.readFile($("input[type='file']", e.target)[0], '')
+			.then(contents => {
+				Ember.set(this.change, 'attachment', contents)
+				return this.deletePost(this.get('change'), false)
+			})
 			.then(() => {
 				this.set('form', Object.assign({}, this.change))
-				return this.handleSubmit()
+				return this.handleSubmit(e)
 			})
 
 	},
@@ -59,13 +63,13 @@ export default Ember.Controller.extend({
 		if(doConfirm && !confirm("Are you sure you want to delete this post?"))
 			return
 		return this.get('ajax').request(this.endpoint + 'posts/' + post.id, {
-	      method: 'DELETE',
-	      data: {key : this.key}
-	    })
-	    .then(result => {
-	    	return this.getPosts()
-	    })
-	    .catch(this.handleError)
+				method: 'DELETE',
+				data: {key : this.key}
+			})
+			.then(result => {
+				return this.getPosts()
+			})
+			.catch(this.handleError)
 	},
 	handleSubmit(e){
 		if(typeof e == 'object')
@@ -81,23 +85,50 @@ export default Ember.Controller.extend({
 		}
 		this.set('loading', true)
 		this.form.key = this.get('key')
-		return this.get('ajax').request(this.endpoint + 'posts', {
-	      method: 'POST',
-	      data: {post : this.form}
-	    })
-	    .then(result => {
-	    	this.set('loading', false)
-	    	return this.getPosts()
-	    })
-	    .then(() => {
-	    	//reset form
-	    	Ember.set(this.form, 'title', '')
-	    	Ember.set(this.form, 'content', '')
-	    })
-	    .catch(e => {
-	    	this.set('loading', false)
-	    	this.handleError(e)
-	    })
+		return this.readFile($('#uploadFile')[0], this.form.attachment)
+			.then(fileContents => {
+				Ember.set(this.form, 'attachment', fileContents)
+				return this.get('ajax').request(this.endpoint + 'posts', {
+					method: 'POST',
+					data: {post : this.form}
+				})
+			})
+			.then(result => {
+				this.set('loading', false)
+				return this.getPosts()
+			})
+			.then(() => {
+				//reset form
+				Ember.set(this.form, 'title', '')
+				Ember.set(this.form, 'content', '')
+				Ember.set(this.form, 'attachment', '')
+				e.target.reset()
+			})
+			.catch(e => {
+				this.set('loading', false)
+				this.handleError(e)
+			})
+	},
+	download(post){
+		var uri = "data:application/octet-stream," + encodeURIComponent(post.attachment),
+			newWindow = window.open(uri, 'attachment')
+	},
+	readFile(file, attachment){
+		return new Promise((resolve, reject) => {
+			if(attachment && attachment != '')
+				return resolve(attachment)
+			file = file.files[0]
+			if(!file)
+				return resolve("")
+			var reader = new FileReader();
+		    reader.readAsText(file, "UTF-8");
+		    reader.onload = function (evt) {
+		        resolve(evt.target.result)
+		    }
+		    reader.onerror = function (evt) {
+		        reject(new Error(evt))
+		    }
+		})
 	},
 	handleError(e){
 		this.set('error', e.toString())
